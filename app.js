7e5b2da8d66a8e6940b0f265577ff5857af248ca
Fernando Carrizo <fer-nando-08@hotmail.com>
2022-09-13 23:56:55 -0300

    first exercise completed

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const movies = require('./movies.json');
  * @returns {Array}
  */
 function getFirstTenMovies(){
-    //complete with your code
+    const firstMovies= movies.slice(0, 10);
+    return firstMovies.map( data => data.title);
 }
 
 /**
