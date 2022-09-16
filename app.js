ca2a349066293c79a46873cf0984267a237b0fc0
Your Name <your-email@website.com>
2022-09-16 02:07:29 -0300

    docs: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * Function that returns the first ten movies of movies.json
  * @returns {Array}
  */
 function getFirstTenMovies(){
@@ -9,7 +9,7 @@ function getFirstTenMovies(){
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * Function that returns an array of movies that contains the genre passed by parameter
  * @param  {String} genre
  * @returns {Array}
  */
