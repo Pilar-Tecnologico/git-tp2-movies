8f670dbb9d5579231d26fe691f39ff61d734ebdf
Ariel Cabeza <cervanteslibch@gmail.com>
2022-09-18 13:12:47 -0300

    Primer ejercicio resuelto

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const movies = require('./movies.json');
  */
 function getFirstTenMovies(){
     //complete with your code
+  const firstTenMovies =  movies.slice(0, 10);
+  return firstTenMovies
 }
 
 /**
@@ -20,9 +22,9 @@ function getFirstTenMovies(){
 
 function main(){
     const firstTenMovies = getFirstTenMovies();
-    const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    // const dramaMovies = getMoviesWithGenre('Drama');
+    console.log(`Only first ten movies`, firstTenMovies);
+    // console.log(`Only Drama movies:\n${dramaMovies}\n`);
 }
 
 main();
