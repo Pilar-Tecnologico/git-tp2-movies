dd59bdda0fdaa6d63c3f84dd9b1cf60518113cd1
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-09-19 11:42:28 -0300

    First Exercise

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const movies = require('./movies.json');
  */
 function getFirstTenMovies(){
     //complete with your code
+    let topTen = []
+    movies.forEach( topTenNames => {
+        if(topTen.length < 10){
+            topTen.push(topTenNames.title)
+        }
+    })
+    return topTen
 }
 
 /**
@@ -20,7 +27,7 @@ function getFirstTenMovies(){
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
+    console.log(`Only first ten movies:`, firstTenMovies);
     console.log(`Only Drama movies:\n${dramaMovies}\n`);
 }
 
