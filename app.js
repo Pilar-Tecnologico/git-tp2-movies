60360e5a1eddb360c541bf9f4e00af78767699be
Cesar Herrera <caherrera2510@gmail.com>
2022-09-20 11:29:53 -0300

    exrcise about first ten moviess

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const movies = require('./movies.json');
  */
 function getFirstTenMovies(){
     //complete with your code
+    const firsttenMovies = movies.slice(0,10);
+    return JSON.stringify(firsttenMovies, null, ' ');
 }
+
 
 /**
  * Here the function apply for a parameter of character string and return an array with all the movies of this genre
