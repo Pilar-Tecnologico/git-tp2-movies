d99d417f5d075b8cd8a10bc407d2465188279525
Campillay Sebastian <israelcampy@gmail.com>
2022-09-23 21:49:07 -0300

    docs: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * This function gets the first ten objects from movies.json and transforms this into a string.
  * @returns {Array}
  */
 function getFirstTenMovies(){
@@ -9,7 +9,7 @@ function getFirstTenMovies(){
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * This function gets movies for genre.
  * @param  {String} genre
  * @returns {Array}
  */
