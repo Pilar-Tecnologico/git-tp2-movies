7c1f30672140af8050c4fc0ccee7cae1336b193e
pablictrad <101837516+pablictrad@users.noreply.github.com>
2022-09-25 19:49:51 -0300

    Resolviendo punto 8

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ function getFirstTenMovies(){
  * @returns {Array}
  */
  function getMoviesWithGenre(genre){
-    //complete with your code
+    let PeliculasPorGenero = movies.filter(pelicula => pelicula.genre == genre);
+    return PeliculasPorGenero;
 }
 
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    console.log(`Only first ten movies:\n${JSON.stringify(firstTenMovies)}\n`);
+    console.log(`Only Drama movies:\n${JSON.stringify(dramaMovies)}\n`);
 }
 
 main();
